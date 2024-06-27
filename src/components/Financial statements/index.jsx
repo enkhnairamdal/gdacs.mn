8b50656620ac9b6e5558b48ef35d00fec8ac0c92
enkhnairamdal <enkhnairamdalbatmagnai@gmail.com>
2024-06-27 15:36:28 +0800

    f

--- src/components/Financial statements/index.jsx
+++ src/components/Financial statements/index.jsx
@@ -1,38 +1,68 @@
-import styles from"./styles.module.css";
+import "./module.css";
 export default function Financial (){
     return(
-        <div className={styles.ficontainer}>
-            <h1>ҮЙЛ АЖИЛЛАГАА,<br /> САНХҮҮГИЙН ТАЙЛАН</h1>
-            <div style={{display:"flex" , flexDirection:"column" , gap:"100px"}}>
-                <div style={{display:"flex", justifyContent:"space-between"}} className={styles.cardtop}>
-                    <div className={styles.cardp}>
-                        <h2>Санхүүгийн тайлан</h2>
-                        <img src="./image/next.png" alt=""/>  
-                    </div>
-                    <div className={styles.cardp}>
-                        <h2>Компанийн үйл ажиллагааны тайлан</h2>
-                        <img src="./image/next.png" alt="" />  
-                    </div>
-                    <div className={styles.cardp}>
-                        <h2>Хувьцаа эзэмшигчдийн хурал</h2>
-                        <img src="./image/next.png" alt="" />  
-                    </div>
-                </div>
-                <div style={{display:"flex" ,justifyContent:"space-between"}}>
-                    <div className={styles.cardp}>
-                        <h2>ТУЗ Ээлжит хурал</h2>
-                        <img src="./image/next.png" alt="" />  
-                    </div>
-                    <div className={styles.cardp}>
-                        <h2>ТУЗ Ээлжит бус хурал</h2>
-                        <img src="./image/next.png" alt="" />  
-                    </div>
-                    <div className={styles.cardp}>
-                        <h2>Компанийн засаглалын гэрчилгээ</h2>
-                        <img src="./image/next.png" alt="" />  
-                    </div>
-                </div>
-            </div>
-        </div>
+        // <div className={styles.ficontainer}>
+        //     <h1>ҮЙЛ АЖИЛЛАГАА,<br /> САНХҮҮГИЙН ТАЙЛАН</h1>
+        //     <div style={{display:"flex" , flexDirection:"column" , gap:"100px"}}>
+        //         <div style={{display:"flex", justifyContent:"space-between"}} className={styles.cardtop}>
+        //             <div className={styles.cardp}>
+        //                 <h2>Санхүүгийн тайлан</h2>
+        //                 <img src="./image/next.png" alt=""/>  
+        //             </div>
+        //             <div className={styles.cardp}>
+        //                 <h2>Компанийн үйл ажиллагааны тайлан</h2>
+        //                 <img src="./image/next.png" alt="" />  
+        //             </div>
+        //             <div className={styles.cardp}>
+        //                 <h2>Хувьцаа эзэмшигчдийн хурал</h2>
+        //                 <img src="./image/next.png" alt="" />  
+        //             </div>
+        //         </div>
+        //         <div style={{display:"flex" ,justifyContent:"space-between"}}>
+        //             <div className={styles.cardp}>
+        //                 <h2>ТУЗ Ээлжит хурал</h2>
+        //                 <img src="./image/next.png" alt="" />  
+        //             </div>
+        //             <div className={styles.cardp}>
+        //                 <h2>ТУЗ Ээлжит бус хурал</h2>
+        //                 <img src="./image/next.png" alt="" />  
+        //             </div>
+        //             <div className={styles.cardp}>
+        //                 <h2>Компанийн засаглалын гэрчилгээ</h2>
+        //                 <img src="./image/next.png" alt="" />  
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+ <div>
+          
+<div className="main-container">
+<h1>ҮЙЛ АЖИЛЛАГАА,<br /> САНХҮҮГИЙН ТАЙЛАН</h1>
+    <div className="card " id="purple"> 
+        <h2>Санхүүгийн тайлан</h2>
+   
+    </div>
+    <div className="card " id="yellow">
+        <h2>Компанийн үйл ажиллагааны тайлан</h2>
+   
+    </div>
+    <div className="card " id="green">
+        <h2>Хувьцаа эзэмшигчдийн хурал</h2>
+   
+    </div>
+    <div className="card  border" id="purple">
+        <h2>ТУЗ Ээлжит хурал</h2>
+   
+    </div>
+    <div className="card  border" id="yellow">
+        <h2>ТУЗ Ээлжит бус хурал</h2>
+   
+    </div>
+    <div className="card border" id="green ">
+        <h2>Компанийн засаглалын гэрчилгээ</h2>
+   
+    </div>
+</div>
+ </div>
     )
 }
